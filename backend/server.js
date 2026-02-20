24f2932655e755984a36df7709478672187f67d4
Aditya Giri <adityagiri83853@gmail.com>
2026-02-20 23:24:24 +0530

    Fix CORS for deployment

--- backend/server.js
+++ backend/server.js
@@ -10,22 +10,53 @@ import userRoutes from "./routes/userRoutes.js";
 import errorHandler from "./middleware/errorMiddleware.js";
 
 dotenv.config();
+
+
 connectDB();
 
 const app = express();
 
-app.use(cors());
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://micro-marketplace-opal.vercel.app"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps, Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  })
+);
+
+// Middleware
 app.use(express.json());
 
+// Routes
 app.use("/auth", authRoutes);
 app.use("/products", productRoutes);
 app.use("/user", userRoutes);
-app.use(errorHandler);
 
+// Root route
 app.get("/", (req, res) => {
-  res.send("API Running");
+  res.send("API Running ");
 });
 
+// Error middleware
+app.use(errorHandler);
+
+// Server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, () =>
+  console.log(` Server running on port ${PORT}`)
+);
